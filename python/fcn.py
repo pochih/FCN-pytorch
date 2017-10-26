bb1b11e4a80a64cfa124362e616bf7a3b34eb201
brianhuang1019 <brianhuang1019@gmail.com>
2017-10-26 19:12:33 +0800

    support vgg11, vgg13, vgg16, vgg19

--- python/fcn.py
+++ python/fcn.py
@@ -157,17 +157,19 @@ class FCNs(nn.Module):
 
 
 class VGGNet(VGG):
-    def __init__(self, pretrained=True, requires_grad=True, remove_fc=False, show_params=False):
-        super().__init__(make_layers(cfg['E']))
+    def __init__(self, pretrained=True, model='vgg16', requires_grad=True, remove_fc=False, show_params=False):
+        super().__init__(make_layers(cfg[model]))
+        self.ranges = ranges[model]
 
         if pretrained:
-            super().load_state_dict(models.vgg19(pretrained=True).state_dict())
+            exec("self.load_state_dict(models.%s(pretrained=True).state_dict())" % model)
+            #super().load_state_dict(models.vgg16(pretrained=True).state_dict())
 
         if not requires_grad:
             for param in super().parameters():
                 param.requires_grad = False
 
-        if remove_fc:  # delete redundant params, can save memory
+        if remove_fc:  # delete redundant fully-connected layer params, can save memory
             del self.classifier
 
         if show_params:
@@ -176,10 +178,9 @@ class VGGNet(VGG):
 
     def forward(self, x):
         output = {}
-        ranges = ((0, 5), (5, 10), (10, 19), (19, 28), (28, 37))
 
-        for idx in range(len(ranges)):
-            for layer in range(ranges[idx][0], ranges[idx][1]):
+        for idx in range(len(self.ranges)):
+            for layer in range(self.ranges[idx][0], self.ranges[idx][1]):
                 x = self.features[layer](x)
             # print(x.size())
             output["x%d"%(idx+1)] = x
@@ -187,9 +188,19 @@ class VGGNet(VGG):
         return output
 
 
+ranges = {
+    'vgg11': ((0, 3), (3, 6),  (6, 11),  (11, 16), (16, 21)),
+    'vgg13': ((0, 5), (5, 10), (10, 15), (15, 20), (20, 25)),
+    'vgg16': ((0, 5), (5, 10), (10, 17), (17, 24), (24, 31)),
+    'vgg19': ((0, 5), (5, 10), (10, 19), (19, 28), (28, 37))
+}
+
 # cropped version from https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
 cfg = {
-    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    'vgg11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'vgg16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'vgg19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
 }
 
 def make_layers(cfg, batch_norm=False):
