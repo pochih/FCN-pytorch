e31516a6e78344bb0ba73889e4204dd2123e07db
ehds <ehds@qq.com>
2019-07-23 09:33:57 +0800

    fix find path
    missing colon after `else` at line 37 in train.py.

--- python/train.py
+++ python/train.py
@@ -34,7 +34,7 @@ print("Configs:", configs)
 
 if sys.argv[1] == 'CamVid':
     root_dir   = "CamVid/"
-else
+else:
     root_dir   = "CityScapes/"
 train_file = os.path.join(root_dir, "train.csv")
 val_file   = os.path.join(root_dir, "val.csv")
